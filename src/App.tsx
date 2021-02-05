21fe3235e233dccff36a3b93bf1fb1b89943373a
MacOS <macos@iMac-Utocka.local>
2021-02-05 11:14:42 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return <></>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
