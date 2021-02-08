cd8b12b892099a7dbd7fa7b60b01424d8e28671e
mannkostir <agabatov7@gmail.com>
2021-02-08 17:32:10 +0600

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,10 @@
 
 import 'react-native-gesture-handler';
 import React from 'react';
-import {StyleSheet} from 'react-native';
 import MainNavigator from './navigators/MainNavigator';
-import {NavigationContainer} from '@react-navigation/native';
-
-declare const global: {HermesInternal: null | {}};
 
 const App = () => {
   return <MainNavigator />;
 };
-
-const styles = StyleSheet.create({});
 
 export default App;
